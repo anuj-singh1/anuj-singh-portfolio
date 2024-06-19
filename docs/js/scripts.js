f15d2fec5bc04e223b44b42b0cd445ebe7d1d84e
Anuj Singh <anuj.singh@zeotap.com>
2024-06-19 12:57:34 +0530

    change form submission webhook

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -103,7 +103,7 @@ document.addEventListener('DOMContentLoaded', function () {
             ]
         }
 
-        const apiUrl = "https://hooks.slack.com/services/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"
+        const apiUrl = "https://hooks.slack.com/services/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"
         const myHeaders = new Headers();
         myHeaders.append("Content-Type", "application/json");
 
